e252be4066820087b24e1a96083c4b647235e530
Raymond <raymond12856@gmail.com>
2018-09-17 21:39:32 +0800

    add some files

--- wsgi_vs_flask/wsgi/app.py
+++ wsgi_vs_flask/wsgi/app.py
@@ -1,1 +1,4 @@
-#app
+def application(environ, start_response):
+	start_response('200 OK', [('Content-Type', 'text/html')])
+	body = '<h1>Hello, %s!</h1>' %(environ['PATH_INFO'][1:] or 'web')
+	return [body.encode('utf-8')]
